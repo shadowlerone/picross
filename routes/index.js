29d6b28e428e6e37f01a3ab450b1e23969748466
shadowlerone <andrae.lewis@gmail.com>
2021-04-05 03:00:59 -0400

    added play endpoint

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,17 @@ var router = express.Router();
 router.get('/test', function (req, res) {
 	res.render('test', {x:10, y:10});
 })
+router.get('/play/:boarddata', function (req,res, next){
+	data = JSON.parse(Buffer.from(req.params.boarddata, 'base64').toString())
+	res.render('test_level', data)
+})
 router.get('/testgame', function (req, res) {
 	res.render('test_level', {x:10, y:10, hints:{"y":[[2,2],[1,3,1],[1,1,1],[3],[9],[1,2,1,2],[1,4,1],[9],[2,1,4],[5]],"x":[[3],[1,2],[5],[8,1],[1,2,4],[7,1],[1,1,3],[3,6],[1,2,2],[1,1,1]]}});
 })
 router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Express' });
 });
+
 
 
 
